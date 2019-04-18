401efaee34e4ad598685138928d0e53876de4940
Anya Vigolajnen <avi26.job@yandex.ru>
2019-04-19 01:24:40 +0300

    работа с svg

---
+++ source/js/rating.js
@@ -0,0 +1,12 @@
+(function () {
+  var container = document.querySelector('.rating');
+  var items = container.querySelectorAll('.rating__item')
+  container.onclick = function (e) {
+    if (!e.target.classList.contains('rating__item--active')) {
+      items.forEach(function (item) {
+        item.classList.remove('rating__item--active');
+      });
+      e.target.classList.add('rating__item--active');
+    }
+  }
+})();
