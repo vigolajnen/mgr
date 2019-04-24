72a87b1ea76da03014c686b944f87c82f74dd53b
vigolajnen.anna <vigolajnen@gmail.com>
2019-04-24 19:07:37 +0300

    правки по слайдерам для главной и шаблону сотрудника

--- source/js/slider.js
+++ source/js/slider.js
@@ -20,6 +20,24 @@ $(document).ready(function() {
         }
       ]
     });
+
+    $("#js-slider-project").slick({
+      dots: true,
+      arrow: false,
+      centerPadding: "40px",
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
   }
 
   window.addEventListener("resize", function() {
@@ -41,46 +59,30 @@ $(document).ready(function() {
           }
         ]
       });
+
+      $("#js-slider-project").slick({
+        dots: true,
+        arrow: false,
+        centerPadding: "40px",
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
     }
   });
 });
 
 
-$("#js-slider-project").slick({
-  dots: true,
-  infinite: true,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        dots: true,
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        dots: true,
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+
 
  $(".slider__for").slick({
    slidesToShow: 1,
