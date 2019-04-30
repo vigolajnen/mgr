ca61a94d691fbdb044a25d8e1ccc823ee7f40343
vigolajnen.anna <vigolajnen@gmail.com>
2019-04-30 12:04:16 +0300

    правки по стр

---
+++ source/js/sort.js
@@ -0,0 +1,8 @@
+var list = document.querySelector('.sort__list');
+var itemActive = document.querySelector(".sort__item.sort__item--active");
+var item = document.querySelectorAll('.sort__item');
+
+itemActive.addEventListener("click", function(evt) {
+  evt.preventDefault();
+
+});
