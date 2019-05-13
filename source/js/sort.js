68193faa6ff9be51e76ef9045a1f826d3423abb4
vigolajnen.anna <vigolajnen@gmail.com>
2019-05-13 14:50:35 +0300

    новые страницы - ЛК - мои компании + инфо пользователя

--- source/js/sort.js
+++ source/js/sort.js
@@ -20,34 +20,36 @@
 
   var activeSortItem = sortList[0];
 
-  box.innerHTML = activeSortItem.name;
+  if (box && list) {
+    box.innerHTML = activeSortItem.name;
+    list.innerHTML = "";
 
-  list.innerHTML = "";
-  sortList.forEach(function(item) {
-    var li = createElementFromHtmlList(
-      '<li class="sort__item" data-value=""></li>'
-    );
-    li.innerHTML =
-      '<button class="button sort__button">' + item.name + "</button>";
-    li.attributes[1].value = item.id;
-    list.appendChild(li);
-  });
-
-  function createElementFromHtmlList(html) {
-    var list = document.createElement("li");
-    list.innerHTML = html;
-    return list.children[0];
-  }
-
-  box.addEventListener("click", function() {
-    if (list.classList.contains("sort__list--active")) {
-      list.classList.remove("sort__list--active");
-    }
-    list.classList.add("sort__list--active");
-    list.addEventListener("click", function() {
-      var target = event.target;
-      box.innerHTML = target.innerHTML;
-      list.classList.remove("sort__list--active");
+    sortList.forEach(function(item) {
+      var li = createElementFromHtmlList(
+        '<li class="sort__item" data-value=""></li>'
+      );
+      li.innerHTML =
+        '<button class="button sort__button">' + item.name + "</button>";
+      li.attributes[1].value = item.id;
+      list.appendChild(li);
     });
-  });
+
+    function createElementFromHtmlList(html) {
+      var list = document.createElement("li");
+      list.innerHTML = html;
+      return list.children[0];
+    }
+
+    box.addEventListener("click", function() {
+      if (list.classList.contains("sort__list--active")) {
+        list.classList.remove("sort__list--active");
+      }
+      list.classList.add("sort__list--active");
+      list.addEventListener("click", function() {
+        var target = event.target;
+        box.innerHTML = target.innerHTML;
+        list.classList.remove("sort__list--active");
+      });
+    });
+  }
 })();
